90695da7ffd2549dcaa298c0b102a02c21e9b500
Flo-Mueller <florian.mueller141@web.de>
2023-11-24 10:55:16 +0100

    copied code for data creation from https://github.com/fwillett/speechBCI/blob/main/AnalysisExamples/makeTFRecordsFromSession.py

--- src/datasets/brain2text.py
+++ src/datasets/brain2text.py
@@ -12,7 +12,7 @@ from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
-from .tokenizer import getTokenizer
+import tokenizer
 
 
 class Brain2TextDataset(Dataset):
@@ -30,17 +30,50 @@ class Brain2TextDataset(Dataset):
             for fileName in os.listdir(dsFolderPath)
         ]
 
-        self.tokenizer = getTokenizer()
+        self.tokenizer = tokenizer.getTokenizer()
 
         self.encodedSentences = []
         self.brainDataSamples: list[torch.Tensor] = []
 
         for dataFile in dataFiles:
-            blockSentences: list[str] = dataFile["sentenceText"]
-            blockBrainData = dataFile["spikePow"][0]
+            n_trials = dataFile['sentenceText'].shape[0]
+            input_features = []
+            transcriptions = []
+            frame_lens = []
 
-            for dataSample, sentence in zip(blockBrainData, blockSentences):
+            #collect area 6v tx1 and spikePow features
+            for i in range(n_trials):    
+                #get time series of TX and spike power for this trial
+                #first 128 columns = area 6v only
+                features = np.concatenate([dataFile['tx1'][0,i][:,0:128], dataFile['spikePow'][0,i][:,0:128]], axis=1)
+
+                sentence_len = features.shape[0]
+                sentence = dataFile['sentenceText'][i].strip()
+
+                input_features.append(features)
+                transcriptions.append(sentence)
+                frame_lens.append(sentence_len)
+
+             #block-wise feature normalization
+            blockNums = np.squeeze(dataFile['blockIdx'])
+            blockList = np.unique(blockNums)
+            blocks = []
+            for b in range(len(blockList)):
+                sentIdx = np.argwhere(blockNums==blockList[b])
+                sentIdx = sentIdx[:,0].astype(np.int32)
+                blocks.append(sentIdx)
+
+            for b in range(len(blocks)):
+                feats = np.concatenate(input_features[blocks[b][0]:(blocks[b][-1]+1)], axis=0)
+                feats_mean = np.mean(feats, axis=0, keepdims=True)
+                feats_std = np.std(feats, axis=0, keepdims=True)
+                for i in blocks[b]:
+                    input_features[i] = (input_features[i] - feats_mean) / (feats_std + 1e-8)
+
+
+            for dataSample in input_features:
                 self.brainDataSamples.append(torch.from_numpy(dataSample))
+            for sentence in transcriptions:
                 self.encodedSentences.append(self.tokenizer.encode(sentence))
 
         assert len(self.encodedSentences) == len(self.brainDataSamples)
