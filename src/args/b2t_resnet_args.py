eae7a68e8182f03d2e0a362635990c58596f390a
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2024-01-08 13:26:26 +0000

    removes window size

--- src/args/b2t_resnet_args.py
+++ src/args/b2t_resnet_args.py
@@ -5,7 +5,6 @@ from src.args.base_args import BaseExperimentArgsModel
 
 
 class B2TResnetArgsModel(BaseExperimentArgsModel):
-    window_size: int = Field(4, description="Actual window size 20 ms * window_size")
     tokenizer: Literal["wav2vec_pretrained", "ours"] = "wav2vec_pretrained"
     wav2vec_checkpoint: Literal[
         "facebook/wav2vec2-base-100h", "facebook/wav2vec2-base-960h"
